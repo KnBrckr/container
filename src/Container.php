7930a8993999c5004676970381c89968d3bb380e
Kenneth J. Brucker <ken@pumastudios.com>
2018-05-14 10:28:04 -0700

    Create Container class

---
+++ src/Container.php
@@ -0,0 +1,77 @@
+<?php
+
+/*
+ * Copyright (C) 2018 Kenneth J. Brucker <ken@pumastudios.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+namespace PumaStudios;
+
+/*
+ *  Protect from direct execution
+ */
+if ( !defined( 'WP_PLUGIN_DIR' ) ) {
+	header( 'Status: 403 Forbidden' );
+	header( 'HTTP/1.1 403 Forbidden' );
+	die( 'I don\'t think you should be here.' );
+}
+
+/**
+ * Extends \Pimple\Container main class
+ *
+ * @author Kenneth J. Brucker <ken@pumastudios.com>
+ */
+class Container extends \Pimple\Container {
+
+	/**
+	 * Get item from the container
+	 *
+	 * @param string $id
+	 *
+	 * @return mixed
+	 */
+	public function get( string $id ) {
+		return $this->offsetGet( $id );
+	}
+
+	/**
+	 * Set item in container
+	 *
+	 * @param string $id
+	 * @param mixed $value
+	 */
+	public function set( string $id, $value ) {
+		return $this->offsetSet( $id, $value );
+	}
+
+	/**
+	 * Execute run method in all classes that supply it
+	 */
+	public function run() {
+		foreach ( $this->keys() as $key ) { // Loop on contents
+			$content = $this->raw( $key );
+			if ( is_callable( $content ) ) {
+				$content = $this[$key];
+			}
+			if ( is_object( $content ) ) {
+				$reflection = new \ReflectionClass( $content );
+				if ( $reflection->hasMethod( 'run' ) ) {
+					$content->run(); // Call run method on object
+				}
+			}
+		}
+	}
+
+}
